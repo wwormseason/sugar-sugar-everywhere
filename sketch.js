58c5d5e48a1f2be22a8cfba9229eb469a72376e3
wwormseason <bookworm.with.a.problem@gmail.com>
2025-11-24 09:47:36 -0500

    ble=hehehejeje
    drawing line sisnt working i suspect its an issue with the strokeWeight

--- sketch.js
+++ sketch.js
@@ -1,7 +1,70 @@
+let cups = [];
+let backgroundColor = 220;
+let secondaryColor = 170;
+let obstacles = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  //cups
+  let c1 = new Cup(100, windowWidth / 2, windowHeight - 90);
+  cups.push(c1);
+
+  //obstacles/platforms
+  let o1 = new Obstacle(windowWidth / 4, windowHeight - 50, 600, 20);
+  obstacles.push(o1);
+  let o2 = new Obstacle(200, 400, 300, 20);
+  obstacles.push(o2);
+
+  strokeWeight(10);
 }
 
 function draw() {
-  background(220);
+  background(backgroundColor);
+
+  cups.forEach((cup) => {
+    fill("white");
+    noStroke();
+    ellipse(cup.x, cup.y + 20, 30, 25);
+    fill(backgroundColor);
+    ellipse(cup.x, cup.y + 20, 18, 13);
+    fill("white");
+    rect(cup.x, cup.y, 30, 40);
+    stroke("black");
+    text(cup.requiredAmount - cup.filledAmount, cup.x + 4, cup.y + 20);
+    noStroke();
+  });
+  obstacles.forEach((obstacle) => {
+    noStroke();
+    fill(secondaryColor);
+    rect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+  });
+}
+
+function mouseDragged() {
+  let lineHue = mouseX - mouseY;
+  stroke(lineHue, 90, 90);
+  line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+class Cup {
+  constructor(requiredAmount, x, y) {
+    this.requiredAmount = requiredAmount;
+    this.x = x;
+    this.y = y;
+    this.filledAmount = 0;
+  }
+
+  fill() {
+    this.filledAmount++;
+  }
+}
+
+class Obstacle {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 }
