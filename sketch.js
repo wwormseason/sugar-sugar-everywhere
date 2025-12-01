048f7342c3ab6b92066ea96bcaf88803539e02c0
wwormseason <bookworm.with.a.problem@gmail.com>
2025-12-01 11:31:26 -0500

    gdf

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
 
   if (level1) {
     //cups
-    c1 = new Cup(100, windowWidth / 2, windowHeight - 90);
+    c1 = new Cup(5, windowWidth / 2, windowHeight - 90);
     cups.push(c1);
 
     //obstacles/platforms
@@ -31,6 +31,24 @@ function setup() {
 
     backgroundColor = "#DC9D00";
     secondaryColor = "#ffd366";
+
+    //sugar.push(new Sugar(200, 0));
+  }
+
+  if (level2) {
+    cups.length = 0;
+    c1 = new Cup(100, 300, 560);
+    cups.push(c1);
+
+    //obstacles/platforms
+    obstacles.length = 0;
+    o1 = new Obstacle(100, 600, 600, 20);
+    obstacles.push(o1);
+    let o2 = new Obstacle(500, 300, 900, 20);
+    obstacles.push(o2);
+
+    backgroundColor = "#3E5F8A";
+    secondaryColor = "#74a5e7ff";
 
     //sugar.push(new Sugar(200, 0));
   }
@@ -87,6 +105,7 @@ function draw() {
     if (level1) {
       level1 = false;
       level2 = true;
+      setup();
     }
   }
 }
@@ -170,28 +189,4 @@ class Sugar {
 // Draw lines with mouse
 function mouseDragged() {
   linesDrawn.push({ x1: pmouseX, y1: pmouseY, x2: mouseX, y2: mouseY });
-}
-// Distance between point and line segment
-function distToSegment(px, py, x1, y1, x2, y2) {
-  let A = px - x1;
-  let B = py - y1;
-  let C = x2 - x1;
-  let D = y2 - y1;
-  let dot = A * C + B * D;
-  let lenSq = C * C + D * D;
-  let param = lenSq !== 0 ? dot / lenSq : -1;
-  let xx, yy;
-  if (param < 0) {
-    xx = x1;
-    yy = y1;
-  } else if (param > 1) {
-    xx = x2;
-    yy = y2;
-  } else {
-    xx = x1 + param * C;
-    yy = y1 + param * D;
-  }
-  let dx = px - xx;
-  let dy = py - yy;
-  return sqrt(dx * dx + dy * dy);
 }
